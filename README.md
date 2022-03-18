07677e02dc7139834f620f6630a555657166006a
Klaus Mueller <kl.mueller@muenchen.de>
2022-03-18 10:53:45 +0100

    init

---
+++ README.md
@@ -0,0 +1,37 @@
+# prometheus alertreceiver webhook2mail
+
+[Openshift alert receivers](https://docs.openshift.com/container-platform/4.9/monitoring/managing-alerts.html#configuring-alert-receivers_managing-alerts) can only be managed by cluster admins in the secret `alertmanager-main` in the namespace `openshift-monitoring`.  
+__paw2mail__ is a webservice that accepts alerts and sends them to an email address.
+The receiver email adress is defined in `label paw2mail`:
+
+```
+apiVersion: monitoring.coreos.com/v1
+kind: PrometheusRule
+metadata:
+  name: foo
+spec:
+  groups:
+    - name: bar
+      rules:
+        - alert: foobar_is_bigger_42
+          expr: 'lhm_count_total{item_type="/count"} > 42'
+          labels:
+            severity: Info
+            paw2mail: jon@example.net
+```
+
+## Setup
+
+Needed environments
+
+```
+export smtp_from=cluster@example.net
+export smtp_host=mail.example.net
+export smtp_port=25
+```
+
+Optional
+```
+export smtp_user=clustermailer
+export smtp_pass=xxxxxxxxxxxxx
+```
---
+++ app.py
@@ -0,0 +1,75 @@
+import sys, os, http.server, socketserver, json, smtplib, ssl
+from json2html import *
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+# python3 paw2mail.py 80
+try:
+    PORT        = int(sys.argv[1])
+except:
+    PORT        = 8080
+
+
+def send_mail(post_body_json):
+
+    try:
+        smtp_from= os.environ['smtp_from']
+        smtp_host= os.environ['smtp_host']
+        smtp_port= os.environ['smtp_port']
+    except:
+        print("smtp setting is missing")
+        return
+
+    smtp_server=smtplib.SMTP(smtp_host, smtp_port)
+    smtp_server.ehlo()
+    smtp_server.starttls()
+    smtp_server.ehlo()
+    try:
+        smtp_server.login(os.environ['smtp_user'], os.environ['smtp_pass'])
+    except:
+        pass
+
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = post_body_json['commonLabels']['alertname']
+    msg['From'] = smtp_from
+    msg['To'] = post_body_json['commonLabels']['paw2mail']
+
+    json_html = json2html.convert(json = post_body_json, table_attributes="style=\"border:1px solid #CCCCCC;\"")
+
+    msg_text = MIMEText( str( post_body_json ), 'plain' )
+    msg_html = MIMEText( str( json_html ), 'html' )
+
+    msg.attach(msg_text)
+    msg.attach(msg_html)
+
+    smtp_server.sendmail(smtp_from, msg['To'] , str(msg))
+    smtp_server.quit()
+
+    return
+
+
+class paw2mail(http.server.SimpleHTTPRequestHandler):
+
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.send_header('Strict-Transport-Security', 'max-age=31536000')
+        self.send_header('Content-Security-Policy', "script-src 'self'")
+        self.end_headers()
+
+    def do_POST(self):
+        try :
+            content_length  = int(self.headers['Content-Length'])
+            post_body       = str(self.rfile.read(content_length).decode('utf-8'))
+
+            if self.path == "/paw2mail" :
+                send_mail( json.loads( post_body ) )
+                self._set_headers()
+                return
+        except:
+            self.send_error(404) # 404 Not Found
+            return
+
+with socketserver.TCPServer(("", PORT), paw2mail) as httpd:
+    print("Http Server Serving at port", PORT)
+    httpd.serve_forever()
